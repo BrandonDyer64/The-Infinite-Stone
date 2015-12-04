5af52173e3eb14cb8b8f6cd891771e105ebd8777
Brandon Dyer <brandondyer64@gmail.com>
2015-12-04 12:12:32 -0500

    Signed-off-by: Brandon Dyer <brandondyer64@gmail.com>

--- The Infinite Stone/android/build.sh
+++ The Infinite Stone/android/build.sh
@@ -1,9 +1,14 @@
+#Build
 echo 'Building...'
 '/home/caninelizard/git/The-Infinite-Stone/The Infinite Stone/gradlew' assembleRelease --stacktrace
+
+#Sign
 echo 'Signing...'
 jarsigner -verbose -storepass 'Chip0100' -sigalg SHA1withRSA -digestalg SHA1 -keystore '/home/caninelizard/git/The-Infinite-Stone/The Infinite Stone/key.keystore' '/home/caninelizard/git/The-Infinite-Stone/The Infinite Stone/android/build/outputs/apk/android-release-unsigned.apk' 'The Infinite Stone'
+
+#Align
 echo 'Aligning...'
 cd '/home/caninelizard/Documents/android-sdk-linux/build-tools/23.0.1'
 ./zipalign -f -v 4 '/home/caninelizard/git/The-Infinite-Stone/The Infinite Stone/android/build/outputs/apk/android-release-unsigned.apk' '/home/caninelizard/git/The-Infinite-Stone/The Infinite Stone/android/build/outputs/apk/outfile.apk'
 echo 'Installing...'
-adb -d install '/home/caninelizard/git/The-Infinite-Stone/The Infinite Stone/android/build/outputs/apk/outfile.apk'
+#adb -d install '/home/caninelizard/git/The-Infinite-Stone/The Infinite Stone/android/build/outputs/apk/outfile.apk'
